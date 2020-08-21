b5635a1a9443cfe792f1cd66a805b17057c389f6
Alfonso Bribiesca <alfonsobries@gmail.com>
2020-08-20 23:52:33 -0500

    prevent logo shrink

--- components/Logo.vue
+++ components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link to="/" class="inline-flex items-center p-3 font-serif text-3xl text-teal-100 transition duration-100 ease-in-out transform bg-gray-900 rounded-lg shadow-inner hover:shadow-lg hover:scale-105 hover:-translate-y-1">
+  <nuxt-link to="/" class="inline-flex items-center flex-shrink-0 p-3 font-serif text-3xl text-teal-100 transition duration-100 ease-in-out transform bg-gray-900 rounded-lg shadow-inner hover:shadow-lg hover:scale-105 hover:-translate-y-1">
     <span class="block w-5 h-5 mr-1 -ml-5 bg-gray-800 rounded-r-full" />
     <span class="flex items-center border-2 border-teal-100 rounded-lg">
       <svg viewBox="0 0 20 20" fill="currentColor" class="w-6 h-6 ml-2"><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" /></svg>
